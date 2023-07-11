b6dc440a5825004735221b8b59a6603163ee2142
TZen3d <139209950+TZen3d@users.noreply.github.com>
2023-07-11 09:09:50 +0200

    Add files via upload

---
+++ layers/layers.js
@@ -0,0 +1,49 @@
+var wms_layers = [];
+
+
+        var lyr_OpenStreetMap_0 = new ol.layer.Tile({
+            'title': 'OpenStreetMap',
+            'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' ',
+                url: 'http://tile.openstreetmap.org/{z}/{x}/{y}.png'
+            })
+        });
+
+        var lyr_GoogleMaps_1 = new ol.layer.Tile({
+            'title': 'Google Maps',
+            'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' ',
+                url: 'http://www.google.cn/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}'
+            })
+        });
+var format_SitesUCPA_2 = new ol.format.GeoJSON();
+var features_SitesUCPA_2 = format_SitesUCPA_2.readFeatures(json_SitesUCPA_2, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_SitesUCPA_2 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_SitesUCPA_2.addFeatures(features_SitesUCPA_2);
+var lyr_SitesUCPA_2 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_SitesUCPA_2, 
+                style: style_SitesUCPA_2,
+                interactive: true,
+                title: '<img src="styles/legend/SitesUCPA_2.png" /> SitesUCPA'
+            });
+
+lyr_OpenStreetMap_0.setVisible(false);lyr_GoogleMaps_1.setVisible(true);lyr_SitesUCPA_2.setVisible(true);
+var layersList = [lyr_OpenStreetMap_0,lyr_GoogleMaps_1,lyr_SitesUCPA_2];
+lyr_SitesUCPA_2.set('fieldAliases', {'id': 'id', 'Site': 'Site', 'webpage': 'webpage', 'Image': 'Image', });
+lyr_SitesUCPA_2.set('fieldImages', {'id': 'TextEdit', 'Site': 'TextEdit', 'webpage': 'TextEdit', 'Image': 'ExternalResource', });
+lyr_SitesUCPA_2.set('fieldLabels', {'id': 'no label', 'Site': 'header label', 'webpage': 'no label', 'Image': 'no label', });
+lyr_SitesUCPA_2.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
